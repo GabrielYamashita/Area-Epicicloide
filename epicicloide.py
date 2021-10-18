26660833cdfef6afaae1ab5adf019625903f0b9f
Gabriel Yamashita <gabriel.yamashita01@gmailcom>
2021-10-18 16:08:08 -0300

    changes variaveis

--- epicicloide.py
+++ epicicloide.py
@@ -2,33 +2,33 @@ import math
 from PIL import Image
 from tqdm import tqdm
 
-image = Image.open("Epicicloide.png")
-epicicloide = image.convert("RGB")
+image = Image.open("Curva.png")
+curva = image.convert("RGB")
 
 width, height = image.size
 areaTotal = width * height
 
 black = (0, 0, 0)
-areaEpicicloide = 0
+areaCurva = 0
 
 loop = tqdm(total=height, position=0, leave=False)
 for i in range(height):
     loop.set_description("Calculating... ".format(i))
     loop.update(1)
     for j in range(width):
-        color = epicicloide.getpixel((j, i))
+        color = curva.getpixel((j, i))
 
         if color == black:
-            areaEpicicloide += 1
+            areaCurva += 1
 loop.close()
 
 areaReal = 15.392 * 16.180
 
-epiReal = areaReal * areaEpicicloide/areaTotal
-epiCalculo = 56*math.pi
+imgReal = areaReal * areaCurva/areaTotal
+Calculo = 56*math.pi
 
-print(f"Método Computacional: {epiReal:.2f}u")
-print(f"Cálculo na mão: {epiCalculo:.2f}u")
+print(f"Método Computacional: {imgReal:.2f}u")
+print(f"Cálculo na mão: {Calculo:.2f}u")
 
-erroPercentual = 100-((epiReal*100)/epiCalculo)
+erroPercentual = 100-((imgReal*100)/Calculo)
 print(f"Erro Percentil: {erroPercentual:.2f}%")
